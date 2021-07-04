54729746ccd39dfb3f8fb2723c51f80fd38e967d
Tamas K. Papp <tkpapp@gmail.com>
2021-07-04 15:35:58 +0200

    whitespace fixes

--- src/AD_ReverseDiff.jl
+++ src/AD_ReverseDiff.jl
@@ -4,9 +4,9 @@
 
 import .ReverseDiff
 
-struct ReverseDiffLogDensity{L, C} <: ADGradientWrapper
+struct ReverseDiffLogDensity{L,C} <: ADGradientWrapper
     ℓ::L
-	compiledtape::C
+    compiledtape::C
 end
 
 """
@@ -16,19 +16,18 @@ end
 Gradient using algorithmic/automatic differentiation via ReverseDiff.
 """
 ADgradient(::Val{:ReverseDiff}, ℓ) = begin
-	f = _logdensity_closure(ℓ)
-	x = rand(dimension(ℓ)) #init random parameters
-	tape = ReverseDiff.GradientTape(f, x)
-	compiledtape = ReverseDiff.compile(tape)
-	ReverseDiffLogDensity(ℓ, compiledtape)
+    f = _logdensity_closure(ℓ)
+    x = rand(dimension(ℓ)) #init random parameters
+    tape = ReverseDiff.GradientTape(f, x)
+    compiledtape = ReverseDiff.compile(tape)
+    ReverseDiffLogDensity(ℓ, compiledtape)
 end
 
 Base.show(io::IO, ∇ℓ::ReverseDiffLogDensity) = print(io, "ReverseDiff AD wrapper for ", ∇ℓ.ℓ)
 
 function logdensity_and_gradient(∇ℓ::ReverseDiffLogDensity, x::AbstractVector{T}) where {T}
     @unpack ℓ, compiledtape = ∇ℓ
-
-	buffer = _diffresults_buffer(ℓ, x)
+    buffer = _diffresults_buffer(ℓ, x)
     result = ReverseDiff.gradient!(buffer, compiledtape, x)
     _diffresults_extract(result)
 end
